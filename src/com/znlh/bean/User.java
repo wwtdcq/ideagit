96fc7a06a03820bb0fe70b9079ce7df5e51aa2b6
wangwei <1304299283@qq.com>
2019-10-24 00:57:59 +0800

    3333

--- src/com/znlh/bean/User.java
+++ src/com/znlh/bean/User.java
@@ -23,5 +23,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("11111111111");
         System.out.println("222222222222");
+        System.out.println("33333333");
     }
 }
